ca7f3bd4ca5ca5868fd1bf4cd46ad0e9ce30a0bf
devereaweaver <devereweaver_dev@icloud.com>
2022-12-13 14:05:15 -0600

    linear search implementation

---
+++ playground/linear-search/linear-search-array.cpp
@@ -0,0 +1,59 @@
+/* Linear search implementation using an array */
+#include <iostream>
+
+int LinearSearch(const int numbers[],int size,  int key)
+{
+	/* Implementation of a linear search through 
+	 * an array for a specified integer. */
+	int i = 0;
+	while (i < size)
+	{
+		if (numbers[i] == key)
+			return i;
+		i++;
+	}
+	return -1;
+}
+
+void indexResult(int key, int keyIndex)
+{
+	if (keyIndex == -1)
+		std::cout << "Key = " << key 
+			<< " not found in array." << std::endl;
+	else
+		std::cout << "Key = " << key 
+			<< " is located at index " << keyIndex
+			<< std::endl;
+}
+
+int main()
+{
+	int numbers[]{54,79,26,91,29,33};
+	int size = sizeof(numbers)/sizeof(int);
+	int key{};
+	int keyIndex{};
+
+	std::cout << "[ ";
+	for (size_t i{}; i < size; ++i)
+		std::cout << numbers[i] << " ";
+	std::cout << "]" << std::endl;
+
+	// best case scenario: key = 54
+	std::cout << "Search for key = 54..." << std::endl;
+	key = 54;
+	keyIndex = LinearSearch(numbers, size, key);
+	indexResult(key, keyIndex);
+
+	// worse case scenario: key = 84 (DNE)
+	std::cout << "\nSearch for key = 84..." << std::endl;
+	key = 84;
+	keyIndex = LinearSearch(numbers, size, key);
+	indexResult(key, keyIndex);
+
+
+	// let's see...
+	std::cout << "\nEnter integer >  ";
+	std::cin >> key;
+	keyIndex = LinearSearch(numbers, size, key);
+	indexResult(key, keyIndex);
+}
